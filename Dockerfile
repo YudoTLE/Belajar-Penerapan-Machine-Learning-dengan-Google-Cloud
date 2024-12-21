46f5068c384e2425490f817baf06a00ddba74f78
YudoTLE <yudojago@gmail.com>
2024-12-21 18:43:29 +0000

    remove unnecessary module and add cors

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM node:18-slim
 
 WORKDIR /app
 
-ENV env prod
-ENV PORT 8080
-ENV MODEL_URL 'https://storage.googleapis.com/submissionmlgc-ariyudopertama-bucket/submissions-model/model.json'
+ENV env=prod
+ENV PORT=8080
+ENV FRONTEND_URL='https://submissionmlgc-ariyudopertama.et.r.appspot.com'
+ENV MODEL_URL='https://storage.googleapis.com/submissionmlgc-ariyudopertama-bucket/submissions-model/model.json'
 
 COPY . .
 
--- src/server/server.mjs
+++ src/server/server.mjs
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import routes from '../server/routes.mjs';
 import loadModel from '../services/load-model.mjs';
 
@@ -7,11 +8,19 @@ const app = express();
 
 const port = process.env.PORT;
 const model = await loadModel();
+const frontend_url = process.env.FRONTEND_URL;
+
+const corsOptions = {
+    origin: frontend_url,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+};
 
 app.set('port', port);
 app.set('model', model);
 
 app.use(routes);
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${ port }`);
