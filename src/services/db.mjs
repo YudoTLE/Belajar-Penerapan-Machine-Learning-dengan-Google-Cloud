d853c0c0cc657109b1a8308143db41ad1fce07a9
YudoTLE <yudojago@gmail.com>
2024-12-22 02:47:51 +0000

    update firestore collection's name

--- src/services/db.mjs
+++ src/services/db.mjs
@@ -3,7 +3,7 @@ import { Firestore } from '@google-cloud/firestore';
 export const addPrediction = async (data) => {
     const db = new Firestore();
 
-    const ref = await db.collection('prediction').add({
+    const ref = await db.collection('predictions').add({
         ...data,
         createdAt: new Date().toISOString()
     });
@@ -14,7 +14,7 @@ export const addPrediction = async (data) => {
 export const getAllPredictions = async () => {
     const db = new Firestore();
 
-    const query = await db.collection('prediction').get();
+    const query = await db.collection('predictions').get();
     return query.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
